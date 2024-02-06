9bcb47b1ac7bef585322aed23413904e414450c0
rekla abderrahim <reklaabderrahim@hotmail.com>
2024-02-06 18:19:22 +0100

    add HPE support

--- docker/scripts/init.sql
+++ docker/scripts/init.sql
@@ -14,7 +14,7 @@
 -- GRANT ALL PRIVILEGES ON DATABASE market TO market_owner;
 --
 
--- CREATE USER market_owner WITH PASSWORD 'market_owner';
+CREATE USER market_owner WITH PASSWORD 'market_owner';
 CREATE USER market_update WITH PASSWORD 'market_update';
 CREATE USER market_select WITH PASSWORD 'market_select';
 CREATE DATABASE market;
